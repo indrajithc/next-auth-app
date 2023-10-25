7f7f5b5513536910cc6ac34a3575f8240c563e5c
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2023-10-25 12:38:48 +0530

    feat: FacebookProvider added

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
@@ -10,6 +11,10 @@ export const options: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
       name: "Credentials",
@@ -47,11 +52,11 @@ export const options: NextAuthOptions = {
   ],
   callbacks: {
     async signIn({ account, profile }) {
-      console.log({account, profile})
+      console.log({ account, profile });
       // if (account.provider === "google") {
       //   return profile.email_verified && profile.email.endsWith("@example.com")
       // }
-      return true // Do different verification for other providers that don't have `email_verified`
+      return true; // Do different verification for other providers that don't have `email_verified`
     },
-  }
+  },
 };
