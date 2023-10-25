e1a5036870abbd13c7779eb8de886e27a3fa5b06
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2023-10-25 12:36:31 +0530

    feat: auth config

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your-cool-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "your-awesome-password",
+        },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "12",
+          name: "Dipin",
+          password: "nextAuth",
+        };
+
+        console.log({ credentials });
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      console.log({account, profile})
+      // if (account.provider === "google") {
+      //   return profile.email_verified && profile.email.endsWith("@example.com")
+      // }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  }
+};
